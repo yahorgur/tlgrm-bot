921deb9bfec4482bf5f287bcd4f637f257542f66
Yahor Zhylinski <yahor@zhylinski.pl>
2025-05-06 17:50:24 +0200

    feat: implement claude requests

--- supabase/functions/handle-tlgm-message/index.ts
+++ supabase/functions/handle-tlgm-message/index.ts
@@ -1,5 +1,5 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
-import { sendOpenAIRequest } from './../lib/openAi.ts';
+import { sendRequest } from './../lib/ai.ts';
 import { sendTelegramMessage } from './../lib/telegram.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.38.4';
 import { trackMessage } from './../lib/trackMessage.ts';
@@ -25,25 +25,21 @@ Deno.serve(async (req: { json: Function }) => {
   }
 
   if (replyIsRequired(record)) {
-    try {
-      const openAiResponse = await sendOpenAIRequest(supabase, record);
-      console.log('openAiResponse', openAiResponse);
-      await sendTelegramMessage(record.chat_id, openAiResponse.message);
-      await trackMessage(supabase, {
-        message: {
-          from: {
-            username: '@myasopivo_bot',
-          },
-          text: openAiResponse.message,
-          chat: {
-            id: record.chat_id,
-          },
+    const aiResponse = await sendRequest(supabase, record);
+    console.log('aiResponse', aiResponse);
+    await sendTelegramMessage(record.chat_id, aiResponse.message);
+    await trackMessage(supabase, {
+      message: {
+        from: {
+          username: '@myasopivo_bot',
         },
-        update_id: record.update_id.toString() + '1',
-      } as RequestBodySchema);
-    } catch (error) {
-      console.error('Error sending request to OpenAI:', error);
-    }
+        text: aiResponse.message,
+        chat: {
+          id: record.chat_id,
+        },
+      },
+      update_id: record.update_id.toString() + '1',
+    } as RequestBodySchema);
   }
 
   return new Response(JSON.stringify({}), { headers: { 'Content-Type': 'application/json' } });
--- supabase/functions/lib/ai.ts
+++ supabase/functions/lib/ai.ts
@@ -1,6 +1,9 @@
 import { PublicMessage, Supabase } from './schemas.ts';
 
 const OPEN_AI_API_KEY = Deno.env.get('OPEN_AI_API_KEY');
+const ANTHROPIC_API_KEY = Deno.env.get('ANTHROPIC_API_KEY');
+
+const USE_ANTHROPIC = Deno.env.get('USE_ANTHROPIC') === 'true';
 
 const buildMessagesHistory = async (supabase: Supabase, chatId: number) => {
   const { data: messages } = await supabase
@@ -23,7 +26,40 @@ const buildMessagesHistory = async (supabase: Supabase, chatId: number) => {
   return messagesForOpenAi;
 };
 
-const sendRequest = async (prompt: string) => {
+const sendAnthropicRequest = async (prompt: string) => {
+  const response = await fetch('https://api.anthropic.com/v1/messages', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': ANTHROPIC_API_KEY,
+      'anthropic-version': '2023-06-01',
+    },
+    body: JSON.stringify({
+      model: 'claude-3-7-sonnet-20250219', // or 'claude-3-sonnet-20240229'
+      max_tokens: 200,
+      messages: [
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+    }),
+  });
+
+  const data = await response.json();
+
+  const message = data?.content?.[0]?.text;
+
+  if (message.message) {
+    console.log('Claude says:', message.message);
+    return message;
+  }
+
+  console.log('Claude says:', JSON.parse(message));
+  return JSON.parse(message);
+};
+
+const sendOpenAiRequest = async (prompt: string) => {
   const response = await fetch('https://api.openai.com/v1/chat/completions', {
     method: 'POST',
     headers: {
@@ -31,7 +67,7 @@ const sendRequest = async (prompt: string) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      model: 'gpt-4o-mini',
+      model: 'gpt-4.5-preview',
       messages: [
         {
           role: 'user',
@@ -52,7 +88,7 @@ const sendRequest = async (prompt: string) => {
   return JSON.parse(match[0]);
 };
 
-export const sendOpenAIRequest = async (supabase: Supabase, record: PublicMessage) => {
+export const sendRequest = async (supabase: Supabase, record: PublicMessage) => {
   const messages = await buildMessagesHistory(supabase, record.chat_id);
 
   console.log(messages);
@@ -61,6 +97,9 @@ export const sendOpenAIRequest = async (supabase: Supabase, record: PublicMessag
   Ты — @myasopivo_bot, один из пяти чуваков в телеграм-чате.
   Всем за 30, все эмигранты из Беларуси, живут в Польше.
   Вы болтаете обо всём: политика, трэш, новости, приколы, иногда технологии.
+
+  Сегодняшняя дата: ${new Date().toISOString()} и это очень важно!!!.
+  Today date: ${new Date().toISOString()} and it's important!!!.
 
   Ты — бот, но с характером. Умный, сдержанно-ироничный, можешь в сарказм, но без занудства и моралей.
   Пиши коротко, по делу или с приколом, как будто пишешь друзьям. Не разжёвывай.
@@ -79,10 +118,15 @@ export const sendOpenAIRequest = async (supabase: Supabase, record: PublicMessag
 
   while (retries < 3) {
     try {
-      return await sendRequest(prompt);
+      console.log('USE_ANTHROPIC', USE_ANTHROPIC);
+      const aiFunction = USE_ANTHROPIC ? sendAnthropicRequest : sendOpenAiRequest;
+
+      const response = await aiFunction(prompt);
+      return response;
     } catch (error) {
-      console.error('Error sending request to OpenAI:', error);
       retries++;
+      console.log('retries', retries);
+      console.error('Error sending request to AI:', error);
     }
   }
 
